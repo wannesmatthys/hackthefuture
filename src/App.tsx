97294e2f169c2e667d57d9d549c423403f30f543
Nicholas Martens <darkdragon.martens@gmail.com>
2021-12-02 10:15:43 +0100

    Create header and Inverted button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Header from './components/base/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-red-500">Test</div>
+    <div className="bg-red-500">
+      <Header title={"HTF"}/>
+    </div>
   )
 }
 
---
+++ src/components/base/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ButtonInverted from './ButtonInverted'
+
+interface IProps {
+  menu: string,
+  title: string,
+}
+
+const Header = (props: IProps): JSX.Element => {
+  const { menu, title } = props;  
+
+  return (
+    <div className="text-white bg-gray-800 flex justify-between items-center p-4">
+      <div className="text-xl font-bold">{title}</div>
+      <div className="flex items-center">
+        <div className="pr-4">Menu</div>
+        <ButtonInverted text="Stop spel" />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
