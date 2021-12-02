f45f426a18c192d7012d1e095566808ba2c794a9
Wannes Matthys <wannesmatthys@gmail.com>
2021-12-02 11:39:31 +0100

    Merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Header from './components/base/Header'
-=======
 import React from "react";
 import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 import router from "./routes";
->>>>>>> main
 
 function App() {
     let routes = useRoutes(router);
@@ -16,18 +9,10 @@ function App() {
 
 const AppWrapper = () => {
   return (
-<<<<<<< HEAD
-    <div className="min-h-screen bg-gray-50">
-      <Header title={"HTF"}/>
-    </div>
-  )
-}
-=======
     <Router>
       <App />
     </Router>
   );
 };
->>>>>>> main
 
 export default AppWrapper;
