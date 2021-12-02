c8dbc63918fab614d7286da8833629b0dcf78cc6
Nicholas Martens <darkdragon.martens@gmail.com>
2021-12-02 11:19:31 +0100

    Create base Game layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Header from './components/base/Header'
-=======
-import React from "react";
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
-import router from "./routes";
->>>>>>> main
+import Button from './components/base/Button'
+import Input from './components/base/Input'
+import Landing from './components/Landing'
+import CreateLobby from './components/CreateLobby'
+import JoinLobby from './components/JoinLobby'
+import Game from './components/Game'
 
 function App() {
-    let routes = useRoutes(router);
-    return routes;
-}
+  const [count, setCount] = useState(0)
 
-const AppWrapper = () => {
   return (
-<<<<<<< HEAD
     <div className="min-h-screen bg-gray-50">
       <Header title={"HTF"}/>
+      <Game />
     </div>
   )
 }
-=======
-    <Router>
-      <App />
-    </Router>
-  );
-};
->>>>>>> main
 
-export default AppWrapper;
+export default App
