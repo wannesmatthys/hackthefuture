86387dc5942d8fd39bc67cfa0875782f357ed279
Nicholas Martens <darkdragon.martens@gmail.com>
2021-12-02 10:49:42 +0100

    Create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import './App.css'
 import Header from './components/base/Header'
 import Button from './components/base/Button'
 import Input from './components/base/Input'
+import Landing from './components/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-red-500">
+    <div className="flex justify-between flex-col min-h-screen">
       <Header title={"HTF"}/>
+      <Landing />
+      <div className="bg-gray-100">Footer? :p</div>
     </div>
   )
 }
