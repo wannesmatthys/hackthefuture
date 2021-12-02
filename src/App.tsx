1ac009f769a4b48cca36a5d3cc8a7780d66a55f5
Wannes Matthys <wannesmatthys@gmail.com>
2021-12-02 10:32:47 +0100

    Create button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Button from './components/base/Button'
+import Input from './components/base/Input'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="m-5">
-      <Button text="Start game" />
+      <Input value="" placeholder="test" />
     </div>
   )
 }
