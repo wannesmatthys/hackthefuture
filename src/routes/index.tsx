cb08afcf60d888191395198398a61d58e7edaed4
Nicholas Martens <darkdragon.martens@gmail.com>
2021-12-02 15:39:09 +0100

    Create endscreen page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import Game from "../components/Game";
 import JoinLobby from "../components/JoinLobby";
 import Landing from "../components/Landing";
 import MissingPage from "../components/MissingPage";
+import EndScreen from "../components/EndScreen";
 
 const router: RouteObject[] = [
   { path: "/", element: <Landing />},
   { path: "/join", element: <JoinLobby />},
   { path: "/lobby/:lobbyCode", element: <Lobby />},
   { path: "/game/:lobbyCode", element: <Game />},
+  { path: "/endscreen", element: <EndScreen />},
   { path: "*", element: <MissingPage />}
 ]
 
